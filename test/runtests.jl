bbb84b06a862681c2f8276d75eedd41f944ebea6
azzaare <jeanfrancois.baffier@gmail.com>
2017-11-01 12:44:02 +0900

    Added citation graphs (arxiv/US patent) from SNAP
    Update indention (readability)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Base.Test
 @testset "SNAP Datasets" begin
   @test @inferred "$(loadsnap(:as_caida))" == "{26475, 53381} undirected simple Int64 graph"
   @test_throws ErrorException loadsnap()
-  @test_throws ErrorException loadsnap(:badsnapxxxxx) 
+  @test_throws ErrorException loadsnap(:badsnapxxxxx)
 end
